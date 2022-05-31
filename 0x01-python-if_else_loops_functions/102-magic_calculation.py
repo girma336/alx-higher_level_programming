2200eb832eda927031c452b621248f5b39f2409f
girma336 <girmatarekegn.gi@gmail.com>
2022-05-31 20:17:51 +0300

    magic

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import dis
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
+    
+dis.dis(magic_calculation)
