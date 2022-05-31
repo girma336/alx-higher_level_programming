d651a0a9bdeeca8965eb2b6a811e4426e80e3237
girma336 <girmatarekegn.gi@gmail.com>
2022-05-31 09:22:07 +0300

    lower or upper

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i != 8:
+                print(f"{i}{j}", end = ", ")
+            else:
+                print(f"{i}{j}")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    if c >= chr(97) and c <= chr(122):
+        return True
+    else:
+        return False
