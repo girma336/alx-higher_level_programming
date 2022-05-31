a186facb25811934161e7687f535d04091e7d730
girma336 <girmatarekegn.gi@gmail.com>
2022-05-31 10:31:51 +0300

    exept e and q

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,2 +1,3 @@
+#!/usr/bin/python3
 for i in range(0, 99):
     print(f"{i} = 0x{format(i,'0x')}")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
         print("0{}".format(i), end= ", ")
