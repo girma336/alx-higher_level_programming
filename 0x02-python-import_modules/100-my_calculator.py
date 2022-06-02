8f464b4ceaf302dd9f94505547c8c91352b55249
girma336 <girmatarekegn.gi@gmail.com>
2022-06-02 20:54:30 +0300

    argv linr

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(0)
+        exit(1)
