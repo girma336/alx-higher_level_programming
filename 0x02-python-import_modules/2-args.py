8a23d008c45fc3bb37197594952fb90d8e6301b0
girma336 <girmatarekegn.gi@gmail.com>
2022-06-02 12:40:56 +0300

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) < 2:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for args in range(1, len(sys.argv)):
-        print("{}: {}".format(args, sys.argv[args]))
-    
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for args in range(1, len(sys.argv)):
+            print("{}: {}".format(args, sys.argv[args]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-length = len(sys.argv)
-if length < 2:
-    print("0")
-else:
-    sum = 0
-    for num in range(1, length):
-        sum += int(sys.argv[num])
-    print(sum)
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length < 2:
+        print("0")
+    else:
+        sum = 0
+        for num in range(1, length):
+            sum += int(sys.argv[num])
+        print(sum)
