3c7891de84e3382291baea8b38cc60b9846dcafc
girma336 <girmatarekegn.gi@gmail.com>
2022-06-06 12:30:33 +0300

    last commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ *print_python_list_info-print the python information with c code
+ *@p:PyObject
+ */
+
+void print_python_list_info(PyObject *p)
+{
+  
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,44 +5,48 @@ void reverse(listint_t **head);
 int compareList(listint_t *head, listint_t *new);
 void reverse(listint_t **head)
 {
-  listint_t *prev = NULL;
-  listint_t *current = *head, *nex_t;
-  while (current != NULL)
+  listint_t *new;
+    listint_t *new2;
+  listint_t *new3 = NULL;
+  new = *head;
+  
+  while (new != NULL)
     {
-      nex_t = current->next;
-      current->next = prev;
-      prev = current;
-      current = nex_t;
+      new2 = new->next;
+      new->next = new3;
+      new3 = new;
+      new = new2;
     }
-  *head = prev;
+  *head = new3;
 }
-
+  
 int compareList(listint_t *head, listint_t *new)
 {
-  listint_t *temp1 = head;
-  listint_t *temp2 = new;
-  while (temp1 != NULL && temp2 != NULL)
+  listint_t *t1 = head;
+  listint_t *t2 = new;
+  while (t1 != NULL && t2 != NULL)
     {
-      if (temp1->n == temp2->n)
+      if (t1->n == t2->n)
 	{
-	  temp1 = temp1->next;
-	  temp2 = temp2->next;
+	  t1 = t1->next;
+	  t2 = t2->next;
 	}
       else
 	return (0);
     }
-  if (temp1 == NULL && temp2 == NULL)
+  if (t1 == NULL && t2 == NULL)
     return (1);
-  return (0);
+
+      return(0);
 }
-	  
+	
+  
 
 int is_palindrome(listint_t **head)
 {
   int res = 1;
   listint_t *slow = *head, *prv_slow = *head, *sec_half;
   listint_t *fast = *head, *mid = NULL;
-
   if (*head != NULL && (*head)->next != NULL)
     {
       while (fast != NULL && fast->next != NULL)
@@ -51,18 +55,19 @@ int is_palindrome(listint_t **head)
 	  prv_slow = slow;
 	  slow = slow->next;
 	}
+
       if (fast != NULL)
 	{
 	  mid = slow;
 	  slow = slow->next;
 	}
-      sec_half = slow;
-
       prv_slow->next = NULL;
+      sec_half = slow;
       reverse(&sec_half);
       res = compareList(*head, sec_half);
 
-      /*reverse(&sec_half);*/
+      /**reverse(sec_half);**/
+
 
       if (mid != NULL)
 	{
@@ -72,5 +77,11 @@ int is_palindrome(listint_t **head)
       else
 	prv_slow->next = sec_half;
     }
-  return (res);
+  return res;
 }
+
+
+      
+      
+
+ 
