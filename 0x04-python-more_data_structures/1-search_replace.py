10e7803122d974b09eae03ab31c9a900880ab92a
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-08 14:37:16 +0300

    sum and set

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if search == i else i for i in my_list] 
+    return [replace if search == i else i for i in my_list]
