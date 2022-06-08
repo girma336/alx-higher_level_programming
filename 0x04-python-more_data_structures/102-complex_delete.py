d33fc464dea48b2a7ead8b04f676a779f954bd74
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-08 16:53:33 +0300

    complex

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary == {}:
+    if a_dictionary == {} or a_directory is None:
         return None
     del_key = []
     for k, v in a_dictionary.items():
