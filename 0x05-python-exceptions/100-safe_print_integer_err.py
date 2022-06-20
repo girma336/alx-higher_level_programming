502f6b9c9e13ee7a5952f1b4b971eec5a3b898b9
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-20 15:36:27 +0300

    adva

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False
+    return True
