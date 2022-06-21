a6234e03bc4d1cacadde60c2c56cfbd8245eecc3
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-21 16:07:48 +0300

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+size
+"""
 
 
 class Square:
+    """Square class with
+    size.
+
+    """
 
     def __init__(self, size):
+
+        """initializes the size of variabl
+        """
         self.__size = size
