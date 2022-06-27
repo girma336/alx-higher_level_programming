7990b7316d916e9b80ad444359d7f7ce3bbdeb9d
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-27 19:17:53 +0300

    area_perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Real definition of a rectangle"""
+
+
+class Rectangle:
+    """Defines the implementation of a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrive the width property"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter for setting"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retreiving the roperty of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """property of area, by
+        retriving the area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Reterive the perimeter
+        of the rectangle
+
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
