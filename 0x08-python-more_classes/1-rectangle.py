d30cb6d6efa604e9a783d4b1cf80e272599e55e9
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-27 18:21:42 +0300

    rectangle_setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-"""
-Write a class Rectangle that defines a rectangle
-
-"""
+"""Real definition of a rectangle"""
 
 
 class Rectangle:
-    """class name define by Rectangle"""
+    """Defines the implementation of a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """intalization of width and height"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """retirive the value of width"""
-        return (self.__width)
+        """Property retriever, for retreiving"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """Property setter, for setter"""
+        """Property setter, for setting"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integre")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """height property"""
-        return (self.__height)
+        """Property retriever, for retreiving
+        the rectangle height
+        """
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """property setter, for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
