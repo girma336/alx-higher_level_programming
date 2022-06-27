d8c1f01ad9a38c7472ec8cad84c230c3fd30395b
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-27 17:25:01 +0300

    rectangle_setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,8 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """Property setter, for setter"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integre")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +32,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """property setter, for height"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
