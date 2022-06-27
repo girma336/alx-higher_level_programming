938a20f3994a8903a11475aae880912e7a09d886
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-27 11:16:13 +0300

    max_and_max

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that print my name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    args:
+
+    first_name:
+    last_name:
+
+    """
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
