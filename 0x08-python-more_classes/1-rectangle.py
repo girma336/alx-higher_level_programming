32f618d48b894ad31894ed975c2b849ac0cf474d
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-27 17:27:20 +0300

    rectangle_setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """intalization of width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
