2bd64df278e4487c8b53f9657c68b16be9458030
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-04 21:12:11 +0300

    isinst

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Write a function that returns True if the object
+is an instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    abj: an argumnebrt
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
