2d9cdb8fd1cf750cf2c8c7493a9ef5a3828302b6
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-04 18:54:41 +0300

    not_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class and inheritance"""
+
+
+class List:
+    """pass"""
+    pass
+
+
+class MyList(List):
+    """ Inherted class"""
+
+    def print_sorted(self):
+        """function publice"""
+
+        print(sorted(self))
