3937a1c2a78eadd61cbed04367d1af0b62c9b753
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-05 23:32:43 +0300

    retrive_all

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a class Student That defines a student by"""
+
+
+class Student:
+    """name of the class was Strudent"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of new student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
