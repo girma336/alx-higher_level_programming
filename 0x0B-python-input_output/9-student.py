43d743ae7ae925fddf132686b29af41cf2ed14e5
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-05 23:10:51 +0300

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a class Student That defines a student by"""
+
+
+
+class Student:
+    """name of the class was Strudent"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of new student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return self.__dict__
