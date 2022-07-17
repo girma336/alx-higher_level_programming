bc291d6a5fda9e454945662a87925b3b4167c461
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-17 10:44:20 +0300

    new_file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,12 +5,23 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    args:
-    __id = id
-    """
+    """represent square"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initalize the given or parameter value"""
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+
         super().__init__(size, size, x, y, id)
 
     @property
