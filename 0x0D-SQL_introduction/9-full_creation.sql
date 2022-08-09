3287e401a3f77522a987961d37f658ed3496959c
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 01:15:13 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,10 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 	);
 
-INSERT INTO second_table (id, name, score) VALUES ('1', 'Jhon', '10');
-INSERT INTO second_table (id, name, score) VALUES ('2', 'Alex', '3');
-INSERT INTO second_table (id, name, score) VALUES ('3', 'Bob', '14');
-INSERT INTO second_table (id, name, score) VALUES ('4', 'George', '8');
+INSERT INTO second_table
+  (id, name, score)
+VALUES 
+  ('1', 'Jhon', '10'),
+  ('2', 'Alex', '3'),
+  ('3', 'Bob', '14'),
+  ('4', 'George', '8');
