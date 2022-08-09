1c6423405f69f45c869f68cb2a61cec05d625faf
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 01:34:24 +0300

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all records with a score >= 10
-SELECT score, name FROM second_table WHERE score >= '10' ORDER BY DESC;
+SELECT score, name FROM second_table WHERE score >= '10' ORDER BY score DESC;
