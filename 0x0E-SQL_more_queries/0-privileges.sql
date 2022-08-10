99f43c7bb87beca49b5ef01711c5f2c5a107509d
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 20:11:08 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- write a script grant all privilege
-GRANT ALL
-ON *.*
-TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
