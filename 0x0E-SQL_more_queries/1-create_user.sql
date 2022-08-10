ac5004d692f19e0afbcad946daa4e61b244bfe2b
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 20:23:48 +0300

    gfhfs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- Write a script create new users
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL 
+ON *.* 
+TO 'user_0d_1'@'localhost';
