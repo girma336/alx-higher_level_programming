a9e62b0c201caea2dc551b4f815f49d60b953367
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 22:16:24 +0300

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- Create databese
+-- creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE htbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE NOT AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-	);
+  id INT UNIQUE AUTO_INCREMENT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+  );
