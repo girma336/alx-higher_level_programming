3cb63bada09c8ebd23e06bcbeebfe9ad042103b6
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 21:24:40 +0300

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- cteate database hbtn_0d_2
-CREATE DATABASES IF NOT EXISTS user_02_0;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
-GRANT SELECT ON user_02_0.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
