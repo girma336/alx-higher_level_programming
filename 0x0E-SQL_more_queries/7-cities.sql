26aa02a527f7e14dd6e7588842445864e98a6aaf
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 22:41:02 +0300

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- create the table database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hubn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
