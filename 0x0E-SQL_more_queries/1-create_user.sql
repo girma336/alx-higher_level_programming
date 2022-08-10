9af40fcb1f288aa5312e3f143529aadb4c5f0223
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 20:26:24 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Write a script create new users
-CREATE USER 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
+GRANT ALL 
 ON *.* 
 TO 'user_0d_1'@'localhost';
