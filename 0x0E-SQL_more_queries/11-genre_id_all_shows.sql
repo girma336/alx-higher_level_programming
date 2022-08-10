a99f6c4af6a9d0414cb356fd0e63b88a2fc9c5ae
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-10 23:01:40 +0300

    fgfk

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- impoer sql data
+-- USE hbtn_0d_tvshows.sql
+-- source hbtn_0d_tvshows.sql
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
