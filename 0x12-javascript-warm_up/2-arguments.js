5f7d1a7bb7fbc4dda5cc969d27164c5b33186673
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-05 21:22:47 +0300

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (args.length > 2) {
-	console.log('Argument found');
-} else {
+if (args.length === 2) {
 	console.log('No argument');
+} else {
+	console.log('Argument found');
 }
