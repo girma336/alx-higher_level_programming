42fbbcac6bdeb675c4986f10729bfd607dd28e87
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-05 22:10:01 +0300

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const name = typeof process.argv[2] === 'undefined' ? 'undefined': process.argv[2];
-const newName = typeof process.argv[3] === 'undefined' ? 'undefined' : process.argv[3];
-console.log(name + ' is ' + newName);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
