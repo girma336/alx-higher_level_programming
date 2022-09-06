34fc9148041935463702d6bc2e9af740fc5457c2
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-06 22:13:24 +0300

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) { this.print(); } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
