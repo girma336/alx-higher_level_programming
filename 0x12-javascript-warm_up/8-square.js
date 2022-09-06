c6811488080490d298ebcf3d73ea9ac8d113d98d
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-06 09:26:49 +0300

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const size = process.argv[2];
 if (isNaN(Math.floor(Number(size)))) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < size; i++) {
-	console.log('X'.repeat(size));
-    }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
