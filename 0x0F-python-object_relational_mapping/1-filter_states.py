55432149ef20f39a3e145a54656dece2e728a85b
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-10 00:00:18 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states")
     for data in cur.fetchall():
-        print(data)
+        if data[1][0] == "N":
+            print(data)
     cur.close()
     db.close()
